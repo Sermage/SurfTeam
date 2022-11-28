7d0628775eb2316ed2be0a2e3f4d3a69391ba893
s.pisakov <s.pisakov@wnfx.ru>
2022-11-28 11:57:09 +0400

    hilt was implemented

---
+++ app/src/main/java/com/sermage/surfteam/App.kt
@@ -0,0 +1,8 @@
+package com.sermage.surfteam
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+}
--- app/src/main/java/com/sermage/surfteam/MainActivity.kt
+++ app/src/main/java/com/sermage/surfteam/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.sermage.surfteam.navigation.NavigationHost
 import com.sermage.surfteam.ui.theme.SurfTeamTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
