97cac00821b4cac253578aeae4249d0576e359ac
s.pisakov <s.pisakov@wnfx.ru>
2022-12-08 12:59:23 +0400

    fixed tab items

--- app/src/main/java/com/sermage/surfteam/data/TabItem.kt
+++ app/src/main/java/com/sermage/surfteam/data/TabItem.kt
@@ -1,22 +1,11 @@
 package com.sermage.surfteam.data
 
-import androidx.compose.runtime.Composable
 import com.sermage.surfteam.R
-import com.sermage.surfteam.ui.screens.main.tabs.AboutAppTabContent
-import com.sermage.surfteam.ui.screens.main.tabs.EmployeesTabContent
-import com.sermage.surfteam.ui.screens.main.tabs.ProjectsTabContent
 
-typealias ComposableFun = @Composable () -> Unit
+sealed class TabItem(val title: Int) {
+    object EmployeesScreenTab : TabItem(title = R.string.employees_screen_tab_title)
 
-sealed class TabItem(val title: Int, val screen: ComposableFun) {
-    object EmployeesScreenTab :
-        TabItem(
-            title = R.string.employees_screen_tab_title,
-            screen = { EmployeesTabContent(employees) })
+    object ProjectsScreenTab : TabItem(title = R.string.projects_screen_tab_title)
 
-    object ProjectsScreenTab :
-        TabItem(title = R.string.projects_screen_tab_title, screen = { ProjectsTabContent() })
-
-    object AboutAppTab :
-        TabItem(title = R.string.about_app_screen_tab_title, screen = { AboutAppTabContent() })
+    object AboutAppTab : TabItem(title = R.string.about_app_screen_tab_title)
 }
